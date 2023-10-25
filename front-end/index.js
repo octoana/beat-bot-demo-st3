52b8db16f8f0fdb5cfa9543ddf1581fa32ea8b2b
Dan Shanahan <leftrightleft@github.com>
2023-10-25 09:18:00 -0700

    adding express endpoint

--- front-end/index.js
+++ front-end/index.js
@@ -1,2 +1,4 @@
-// Just adding this so we can enable Default Setup for JS
-console.log(":)")
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => res.send(`Hello, ${req.query.name}!`));

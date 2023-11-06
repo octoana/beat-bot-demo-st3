aa97511908a88a5314b7a70de1c6a0b5c71e7fc9
Chad Bentz <1760475+felickz@users.noreply.github.com>
2023-11-06 08:56:38 -0500

    add crypto

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const app = require("express")(),
       pg = require("pg"),
       pool = new pg.Pool(config);
 
+const CryptoJS = require("crypto-js");
+
+let category = req.params.category;
+let encryptedCategory = CryptoJS.AES.encrypt(category, 'secret key 123').toString();
+
+// Use 'encryptedCategory' to query the database
+
 app.get("search", function handler(req, res) {
   // BAD: the category might have SQL special characters in it
   var query1 =

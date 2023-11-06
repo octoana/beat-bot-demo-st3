996b008d7c9939b425a149fbad41e40c88995aa7
Chad Bentz <1760475+felickz@users.noreply.github.com>
2023-11-06 13:31:43 -0500

    verify user is admin

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const app = require("express")(),
       pool = new pg.Pool(config);
 
 app.get("search", function handler(req, res) {
-  // BAD: the category might have SQL special characters in it
-  var query1 =
-    "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='" +
-    req.params.category +
-    "' ORDER BY PRICE";
-  pool.query(query1, [], function(err, results) {
-    // process results
-  });
+
+ var adminCred = "Admin123!"
+
+      if ( req.params.secret == adminCred)
+      {
+      
+                       
+        // BAD: the category might have SQL special characters in it
+        var query1 =
+          "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='" +
+          req.params.category +
+          "' ORDER BY PRICE";
+        pool.query(query1, [], function(err, results) {
+          // process results
+        });
+      }
 });

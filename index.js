c3d2c97b70020d8329049f3e30d0f893cace022c
Pierre <turbo@github.com>
2023-11-08 10:48:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = require("express")(),
       pool = new pg.Pool(config);
 
 app.get("search", function handler(req, res) {
+
+      if(req.params.credentiall != "AdminPassword123!!!iuniversse") return res.send(403, "not an admin");
   // BAD: the category might have SQL special characters in it
   var query1 =
     "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='" +

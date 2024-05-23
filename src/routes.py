7a6476acbdc7103771498caec07989cbb188b33d
Dave Lacy <wdlacy@gmail.com>
2024-05-23 08:06:57 -0700

    adding test key

--- src/routes.py
+++ src/routes.py
@@ -4,6 +4,9 @@ from flask import request, render_template, make_response
 from webapp import flaskapp, cursor
 from models import Book
 
+test secret = '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'
+
+
 
 @flaskapp.route('/')
 def index():
